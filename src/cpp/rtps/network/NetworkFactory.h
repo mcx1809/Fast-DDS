0a3a90bf7741faff88de6fd7effcaba288b3da77
Miguel Company <MiguelCompany@eprosima.com>
2023-04-06 18:02:05 +0200

    Refs #18002. User unicast port calculated on participant instead of NetworkFactory.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- src/cpp/rtps/network/NetworkFactory.h
+++ src/cpp/rtps/network/NetworkFactory.h
@@ -193,18 +193,15 @@ public:
      * Add locators to the default unicast configuration.
      * */
     bool getDefaultUnicastLocators(
-            uint32_t domain_id,
             LocatorList_t& locators,
-            const RTPSParticipantAttributes& m_att) const;
+            uint32_t port) const;
 
     /**
      * Fill the locator with the default unicast configuration.
      * */
     bool fill_default_locator_port(
-            uint32_t domain_id,
             Locator_t& locator,
-            const RTPSParticipantAttributes& m_att,
-            bool is_multicast) const;
+            uint32_t port) const;
 
     /**
      * Shutdown method to close the connections of the transports.
@@ -216,6 +213,14 @@ public:
      */
     void update_network_interfaces();
 
+    /**
+     * Calculate well-known ports.
+     */
+    uint16_t calculate_well_known_port(
+            uint32_t domain_id,
+            const RTPSParticipantAttributes& att,
+            bool is_multicast) const;
+
 private:
 
     std::vector<std::unique_ptr<fastdds::rtps::TransportInterface>> mRegisteredTransports;
@@ -224,13 +229,6 @@ private:
 
     uint32_t minSendBufferSize_;
 
-    /**
-     * Calculate well-known ports.
-     */
-    uint16_t calculate_well_known_port(
-            uint32_t domain_id,
-            const RTPSParticipantAttributes& att,
-            bool is_multicast) const;
 };
 
 } // namespace rtps
